01fc5d894f9d09e7c98e1e9953c65c3af10bf5f5
DAT3042005 <147588577+DAT3042005@users.noreply.github.com>
2023-12-06 10:31:32 +0700

    Add files via upload

---
+++ TH6.cpp
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+struct sinh vien
+{ 
+   char  Masv[10];
+   char  Hotensv[30];
+   char  PHSI[10];
+   int   Namsinh;
+   char  Nganhhoc[30];
+   float Kquacuoikhoa;
+   char  Xeploai[20];
+   char  Quequan{30};
+};
+typedef struct SinhVien SINHVIEN
+void TIMSV(SINHVIEN a[], int n);
+void INGX(SINHVIEN a[], int n);
+void Sapxep(SINHVIEN a[], int n);  
+void Nhapsv(SINHVIEN &[]);
+void NhapsvN(SINHVIEN a[], int n)
+void Xuatsv(SINHVIEN)
+void XuatsvN(SINHVIEN a[], int n)
+int main()
+{  int n,key;
+   SINHVIEN x,a[10];
+   while(true)
+   {  
+ system("cls");
+ printf("******************************************************\n");
+ printf("**      CHUONG TRINH QUAN LY SINH VIEN              **\N");
+ printf("**        1. Nhap DS sinh vien                      **\n");
+ printf("**        2. In danh sach sinh vien                 **\n");
+ printf("**        3. Sap xep theo kq cuoikhoa               **\n");
+ printf("**        4. In sinh vien Gioi, Xuat sac            **\n");
+ printf("**        5. Tim kiem sinh vien theo MASV           **\n");
+ printf("**        0 . Thoat                                 **\n");
+ printf("******************************************************\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
